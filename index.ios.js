9fe7eb78ca944663737e6afe0d271813addae50e
johnrenko <vishnou@gmail.com>
2016-10-31 22:42:57 +0100

    Added Tachyons and Router

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,49 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Sample Firebase & React Native App
+ * https://github.com/davideast/firebase-react-native-sample
  */
+'use strict';
 
-import React, { Component } from 'react';
-import {
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+import * as firebase from 'firebase';
+import NativeTachyons from "react-native-style-tachyons";
+import {styles as s} from "react-native-style-tachyons";
+
+const {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  TouchableHighlight,
+  AlertIOS,
+} = ReactNative;
 
-export default class museumListing extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAJO4FTAqReHONa3n4FvNo9R9u8RghAZJc",
+  authDomain: "museumlisting.firebaseapp.com",
+  databaseURL: "https://museumlisting.firebaseio.com",
+  storageBucket: "museumlisting.appspot.com",
+  messagingSenderId: "968555127105"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+NativeTachyons.build({
+   /* REM parameter is optional, default is 16 */
+   rem: 16
+}, StyleSheet);
+
+  class museumListing extends Component {
+
+    render() {
+      return (
+        <View style={[s.flx_i, s.flx_row, s.jcc, s.aic, s.ma3, s.bb, s.max_h3, s.mh0]}>
+          <Text style={[s.f4]}>Test </Text>
+        </View>
+      )
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('museumListing', () => museumListing);
